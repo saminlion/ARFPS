6cc39f0e51e8ededf9be9db2f4e69dc511498d6b
SMH <swh5246@gmail.com>
2016-08-31 13:40:39 +0900

    Gun Shooting Commit

---
+++ Assets/02.Scripts/ButtonManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour
+{
+    private csGun gun;
+
+    // Use this for initialization
+    void Awake()
+    {
+        gun = GameObject.Find("Gun").GetComponent<csGun>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+
+    public void Reload()
+    {
+        
+    }
+
+    public void Shoot()
+    {
+        gun.Shooting();
+    }
+}
---
+++ Assets/02.Scripts/csGun.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csGun : MonoBehaviour
+{
+    [Header("Shooting Setup")]
+    public float shootingDelay = 3.0f;
+    public float shootingPower = 15.0f;
+    private float shootingDelayPrivate = 0.0f;
+
+    [Header("Gun Setup")]
+    public int gunChoose = 0;
+    public GameObject bulletPrefab;
+    public GameObject gun;
+    public GameObject firePos;
+
+    // Use this for initialization
+    void Awake()
+    {
+        shootingDelayPrivate = shootingDelay;
+
+        gun = GameObject.FindGameObjectWithTag("Gun");
+
+
+        firePos = gun.GetComponentInChildren<GameObject>();
+            
+
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        shootingDelayPrivate -= Time.deltaTime;
+//
+//        if (shootingDelayPrivate < 0)
+//        {
+//            shootingDelayPrivate = shootingDelay;
+//        }
+    }
+
+    public void Shooting()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePos.transform.position, firePos.transform.rotation) as GameObject;
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootingPower, ForceMode.Impulse);
+    }
+}
