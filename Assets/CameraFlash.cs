32a587cc367a5a306d409dc7e60f17d573e2b491
SMH <swh5246@gmail.com>
2016-08-31 09:46:03 +0900

    First Commit

---
+++ Assets/CameraFlash.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+public class CameraFlash : MonoBehaviour {
+	public Light spotLight;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void FlashOn()
+	{
+		CameraDevice.Instance.SetFlashTorchMode (true);
+		spotLight.enabled = true;
+	}
+	public void FlashOff()
+	{
+		CameraDevice.Instance.SetFlashTorchMode (false);
+		spotLight.enabled = false;
+		}
+}
